e7d8e67d83bf62c77af92127585665bffdc43f07
Ome Gak <omegak@gmail.com>
2013-05-01 21:52:35 +0200

    Script for activating command line tool in OS X

---
+++ command-support-osx.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# /usr/bin/subl instead of /usr/local/bin/subl to avoid Homebrew install folder
+ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/bin/subl
